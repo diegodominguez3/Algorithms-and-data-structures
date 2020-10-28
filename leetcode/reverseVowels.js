bf02e2cbf33a3e1a34e3c249725d2e04892c65ac
Diego <diegodominguez333@gmail.com>
2020-10-28 08:42:54 -0700

    reverse vowels mock interview leetcode

---
+++ leetcode/reverseVowels.js
@@ -0,0 +1,23 @@
+function reverseVowels(s) {
+    let start = 0; 
+    let end = s.length - 1; 
+    const vowels = "aeiou"; 
+    s = s.split(''); 
+    
+    while(start < end) {
+        if(vowels.includes(s[start]) && vowels.includes(s[end])) {
+            console.log('caca'); 
+            const temp = s[start]; 
+            s[start] = s[end]; 
+            s[end] = temp; 
+            start++; 
+            end--; 
+        }
+        if(!vowels.includes(s[start])) start++; 
+        if(!vowels.includes(s[end])) end--; 
+    }
+    
+    return s.join(''); 
+}
+
+console.log(reverseVowels("hello")); 

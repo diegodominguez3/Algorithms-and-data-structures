0b8c9e5d6eb2cda8f0512b028b2f5929faca7bb3
Diego Dominguez <diegodominguez333@gmail.com>
2020-11-18 19:47:22 -0700

    binary search recursive

--- AlgoExpert/Easy/BinarySearch.js
+++ AlgoExpert/Easy/BinarySearch.js
@@ -13,3 +13,19 @@ function binarySearch(array, target) {
     }
     return -1; 
 }
+
+function binarySearch2(array, target ) {
+    // Write your code here.
+      return binarySearchHelper(array, target, 0 , array.length - 1); 
+  }
+function binarySearchHelper (array, target, left, right){
+    if(left > right) return -1; 
+    const midpoint = Math.floor((left + right)/2); 
+    const midvalue = array[midpoint]; 
+    if(target === midvalue) return midpoint;
+    else if (target < midvalue) {
+        return binarySearchHelper(array, target, left, midpoint - 1); 
+    } else {
+        return binarySearchHelper(array, target, midpoint + 1, right); 
+    }
+}

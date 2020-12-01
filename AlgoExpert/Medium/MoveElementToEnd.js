f0c1267e59dc96b6d7ad77240ab13847e078c4c5
Diego Dominguez <diegodominguez333@gmail.com>
2020-11-30 20:04:21 -0700

    move element ot end

--- AlgoExpert/Medium/MoveElementToEnd.js
+++ AlgoExpert/Medium/MoveElementToEnd.js
@@ -1,17 +1,50 @@
-//naive solution O(n^2)
+//my solution O(n) time (1) space 
 function moveElementToEnd(array, toMove) {
     // Write your code here.
-    let splice_counter = 0;
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] === toMove) {
-            array.splice(i, 1);
-            splice_counter++;
-            i--;
+    let left = 0;
+    let right = array.length - 1;
+    while (left < right) {
+        const left_element = array[left];
+        const right_element = array[right];
+        if (left_element === toMove && right_element !== toMove) {
+            swap(left, right, array);
+            left++;
+            right--;
+        } else if (left_element !== toMove && right_element !== toMove) {
+            left++;
+        } else {
+            right--;
         }
-    }
-    for (let i = 0; i < splice_counter; i++) {
-        array.push(toMove);
     }
 
     return array;
+
+    function swap(index1, index2, array) {
+        const temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
+    }
 }
+
+//Other solution O(n) time O(1) space
+function moveElementToEnd(array, toMove) {
+    // Write your code here.
+      let left = 0; 
+      let right = array.length - 1;
+      while(left < right) {
+          while(left < right && array[right] == toMove) {
+              right--;
+          } 
+          if(array[left] === toMove) {
+              swap(left, right, array); 
+          }
+          left++; 
+      }
+      return array;
+      
+      function swap(index1, index2, array) {
+          const temp = array[index1]; 
+          array[index1] = array[index2]; 
+          array[index2] = temp; 
+      }
+  }

8e5a626640d3743cf14ae929566f7236febd7d11
Diego Dominguez <diegodominguez333@gmail.com>
2020-12-27 14:39:56 -0700

    string halves are alike

---
+++ leetcode/StringHalvesAreAlike.js
@@ -0,0 +1,18 @@
+function halvesAreAlike(s) {
+    const vowels = new Set(['a', 'e', 'i', 'o', 'u', 'A','E', 'I', 'O', 'U']); 
+    let left = 0; 
+    let right = s.length - 1; 
+    let counter = 0; 
+    while(left < right) {
+        if(vowels.has(s[left])) counter ++; 
+        if (vowels.has(s[right])) counter --; 
+        left++;
+        right --; 
+        console.log(counter);
+    }
+
+    return counter === 0; 
+
+};
+
+console.log(halvesAreAlike('book')); 

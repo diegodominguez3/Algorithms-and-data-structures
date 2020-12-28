5f55bf60a78b19258b91ab3b7ca38bba3ec4f9a6
Diego Dominguez <diegodominguez333@gmail.com>
2020-12-28 15:12:56 -0700

    Find Successor BST

--- AlgoExpert/Medium/FindSuccessor.js
+++ AlgoExpert/Medium/FindSuccessor.js
@@ -1,18 +1,17 @@
 function findSuccessor(tree, node) {
     // Write your code here.
-    let nodes_arr = [];
-    inOrderTraverse(tree, nodes_arr);
-    for (let i = 0; i < nodes_arr.length; i++) {
-        if (nodes_arr[i] === node) return nodes_arr[i];
-    }
-    return -1;
-
-    function inOrderTraverse(tree, array) {
-        if (tree !== null) {
-            inOrderTraverse(tree.left, array);
-            array.push(tree.value);
-            inOrderTraverse(tree.right, array);
-        }
-        return array;
-    }
-}
+      const nodes_arr = inOrderTraverse(tree); 
+      for(let i = 0; i < nodes_arr.length; i++) {
+          if(i === nodes_arr.length - 1) return null; 
+          if(nodes_arr[i] === node) return nodes_arr[i + 1]; 
+      }
+      
+    function inOrderTraverse(tree,array = []) {
+          if(tree !== null) {
+              inOrderTraverse(tree.left, array); 
+              array.push(tree);
+              inOrderTraverse(tree.right, array); 
+          }
+          return array; 
+      }
+  }

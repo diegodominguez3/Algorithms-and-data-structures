74f259f49138c1ce1021e4517466c698aa77909f
Diego Dominguez <diegodominguez333@gmail.com>
2021-01-14 21:18:21 -0700

    min heap

---
+++ AlgoExpert/Medium/MinHeapConstruction.js
@@ -0,0 +1,35 @@
+class MinHeap {
+  //currentnode  -> i 
+  //child 1 -> 2i + 1
+  //child 2 -> 2i + 2 
+  //parent node -> floor((i-1)/2)
+
+  constructor(array) {
+    this.heap = this.buildHeap(array);
+  }
+
+  buildHeap(array) {
+    // Write your code here.
+    
+  }
+
+  siftDown() {
+    // Write your code here.
+  }
+
+  siftUp() {
+    // Write your code here.
+  }
+
+  peek() {
+    // Write your code here.
+  }
+
+  remove() {
+    // Write your code here.
+  }
+
+  insert(value) {
+    // Write your code here.
+  }
+}

d0b99091ac0f1e208d6665c48355b94bf5b986f2
Diego Dominguez <diegodominguez333@gmail.com>
2021-01-19 15:13:23 -0700

    leetcode linked lists

---
+++ leetcode/Easy/PalindromeLinkedList.js
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+function isPalindrome(head) {
+  let fast = head; 
+  let slow = head; 
+  
+  while(fast !== null && fast.next !== null) {
+    fast = fast.next.next;
+    slow = slow.next;
+  }
+  slow = reversed(slow); 
+  fast = head; 
+
+  while(slow !== null) {
+    if(slow.val !== fast.val) return false;
+    slow = slow.next; 
+    fast = fast.next;
+  }
+
+  return true;
+
+  function reversed(node) {
+    let prev = null; 
+    while(node !== null) {
+      let next = node.next;
+      node.next = prev; 
+      prev = node;
+      node = next;
+    }
+    return prev; 
+  }
+}
---
+++ leetcode/Easy/ReverseLinkedList.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+
+ // 1 -> 2 -> 3 -> 4 -> 5 -> null
+ // 5 -> 4 -> 3 -> 2 -> 1 -> null
+function reverseList(head) {
+  let prev = null; 
+  while(head !== null) {
+    let next = head.next;
+    head.next = prev; 
+    prev = head;
+    head = next; 
+  }
+  return prev;
+}

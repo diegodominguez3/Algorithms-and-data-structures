db38aff45270115eb4cebe1d9b4c733f29515db1
Diego Dominguez <diegodominguez333@gmail.com>
2021-01-20 16:03:49 -0700

    task assignment

---
+++ AlgoExpert/Medium/TaskAssignment.js
@@ -0,0 +1,14 @@
+//greedy algorithm
+function taskAssignment(k, tasks) {
+  // Write your code here.
+	const sorted_tasks = [...tasks].sort((a,b) => a - b); 
+	let output = []; 
+	let left = 0; 
+	let right = sorted_tasks.length - 1;
+	while(left < right) {
+		output.push([left,right]); 
+		left ++; 
+		right --; 
+	}
+	return output; 
+}

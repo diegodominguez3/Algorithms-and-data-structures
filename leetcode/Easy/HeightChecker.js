5559974fc129d5dde640405cc317bea2e021b6a3
Diego Dominguez <diegodominguez333@gmail.com>
2021-01-31 18:48:42 -0700

    remove duplicates from linked list

---
+++ leetcode/Easy/HeightChecker.js
@@ -0,0 +1,10 @@
+function heightChecker (heights) {
+  const sorted = [...heights].sort((a,b) => a - b); 
+  let count = 0; 
+  for(let i = 0; i < heights.length; i ++) {
+      if(sorted[i] !== heights[i]) {
+          count += 1; 
+      }
+  }
+  return count; 
+};

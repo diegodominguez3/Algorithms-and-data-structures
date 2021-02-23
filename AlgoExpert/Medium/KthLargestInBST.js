eef9b377ef2676663351bfc3d5f33f41ea0f2ddb
Diego Dominguez <diegodominguez333@gmail.com>
2021-02-22 18:29:41 -0700

    kth largest value in BST

---
+++ AlgoExpert/Medium/KthLargestInBST.js
@@ -0,0 +1,15 @@
+//My approach inorder traversal
+// O(n) time O(n) space
+function findKthLargestValueInBst(tree, k) {
+  // Write your code here.
+  const largests = dfsRight(tree);
+  return largests[k - 1];
+}
+
+function dfsRight(node, array = []) {
+  if (node === null) return;
+  dfsRight(node.right, array);
+  array.push(node.value);
+  dfsRight(node.left, array);
+  return array;
+}
